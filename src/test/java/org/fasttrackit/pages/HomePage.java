59c6293b88d1c19561b0e3ab5ea66b4583e95635
Debora Maxinas <md.dumuta@gmail.com>
2023-01-09 21:23:13 +0200

    Revert "Serenity partea 1"
    This reverts commit aaea9d1337a213012a81e371b99ec424b5fd4aae.

--- src/test/java/org/fasttrackit/pages/HomePage.java
+++ src/test/java/org/fasttrackit/pages/HomePage.java
@@ -4,7 +4,6 @@ import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.core.pages.WebElementFacade;
 import net.thucydides.core.annotations.DefaultUrl;
-import org.openqa.selenium.WebElement;
 
 @DefaultUrl("http://testfasttrackit.info/selenium-test/")
 public class HomePage extends PageObject {
@@ -15,20 +14,12 @@ public class HomePage extends PageObject {
     @FindBy(css = "[title='Log In']")
     private WebElementFacade loginLink;
 
-    @FindBy(css="[title='Register']")
-    private WebElementFacade registerLink;
-
-
     public void clickOnAccountButton(){
         clickOn(accountButton);
     }
 
     public void clickOnLoginLink(){
         clickOn(loginLink);
-    }
-
-    public void clickOnRegisterLink(){
-        clickOn(registerLink);
     }
 
 
